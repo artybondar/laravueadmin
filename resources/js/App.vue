6a60b08e4b432f14a18438dbfb30b62e728c92f5
Arty <bondaraf@gmail.com>
2020-08-14 15:33:05 +0300

    install && start to work

---
+++ resources/js/App.vue
@@ -0,0 +1,12 @@
+<template>
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="card-body">
+                    <h1>App Vue</h1>
+                    <h2>Get Start!</h2>
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
